bcb931a17c0ddb4698fcd50dde064c8147a718d1
Davi <davihgs53@gmail.com>
2023-12-14 17:55:33 -0300

    Creating a REST Api with Gin

---
+++ gin-rest-api/controllers/controller.go
@@ -0,0 +1,93 @@
+package controllers
+
+import (
+	"gin-rest-api/database"
+	"gin-rest-api/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func GetAllStudents(c *gin.Context) {
+	var students []models.Student
+
+	database.DB.Find(&students)
+
+	c.JSON(http.StatusOK, students)
+}
+
+func GetStudentById(c *gin.Context) {
+	var student models.Student
+	id := c.Params.ByName("id")
+	database.DB.Find(&student, id)
+
+	if student.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"Not found": "Student not found",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, student)
+}
+
+func FindStudentByCPF(c *gin.Context) {
+	var student models.Student
+	cpf := c.Param("cpf")
+
+	database.DB.Where(&models.Student{CPF: cpf}).First(&student)
+
+	if student.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"Not Found": "Student CPF not Found",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, student)
+}
+
+func CreateNewStudent(c *gin.Context) {
+	var newStudent models.Student
+
+	err := c.ShouldBindJSON(&newStudent)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	database.DB.Create(&newStudent)
+	c.JSON(http.StatusOK, newStudent)
+}
+
+func UpdateStudent(c *gin.Context) {
+	var student models.Student
+	id := c.Params.ByName("id")
+	database.DB.First(&student, id)
+
+	err := c.ShouldBindJSON(&student)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	database.DB.Model(&student).UpdateColumns(student)
+	c.JSON(http.StatusOK, student)
+}
+
+func DeleteStudent(c *gin.Context) {
+	var student models.Student
+	id := c.Params.ByName("id")
+
+	database.DB.Delete(&student, id)
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Student removed successfully",
+	})
+}
